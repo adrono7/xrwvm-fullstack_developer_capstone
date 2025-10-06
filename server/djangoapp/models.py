e85065821cb7f79c4abaa9ca2d2a0e29df028426
adrono7 <adam.noack@gmail.com>
2025-10-06 17:21:39 -0400

    python formatting 1

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
 class CarMake(models.Model):
@@ -21,8 +22,9 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
+
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),

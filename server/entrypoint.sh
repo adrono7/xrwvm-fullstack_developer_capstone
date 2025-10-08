b4b6656b1db7956d3e43731c035d8c713965113c
adrono7 <adam.noack@gmail.com>
2025-10-08 18:12:28 -0400

    added superuser creation to entrypoint

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 # Make migrations and migrate the database.
-echo "Making migrations and migrating the database. "
+echo "Making migrations and migrating the database."
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-sh ./testauthuser.sh root root@root.io root root y --noinpu
+
+# Create superuser if it doesn't exist
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); \
+if not User.objects.filter(username='admin').exists(): \
+    User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')" | python manage.py shell
+
 exec "$@"

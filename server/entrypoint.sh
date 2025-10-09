02b1453771d5bdbe56e02f635a26f8cf27646e45
adrono7 <adam.noack@gmail.com>
2025-10-09 14:50:08 -0400

    su removed from entrypoint

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -5,10 +5,4 @@ echo "Making migrations and migrating the database."
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-
-# Create superuser if it doesn't exist
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); \
-if not User.objects.filter(username='admin').exists(): \
-    User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')" | python manage.py shell
-
 exec "$@"
